d7259c702aa49e65896b5433fbf3fb529b61835d
donnyangel.caballero <dogncamo@suramericana.com.co>
2024-06-11 17:29:47 -0500

    +++

--- extension.js
+++ extension.js
@@ -2,69 +2,151 @@ const vscode = require('vscode');
 const { createClient } = require('@supabase/supabase-js');
 const { SUPABASE_URL, SUPABASE_ANON_KEY } = require('./config')
 
-// Configuración de Supabase
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 let isProgrammaticChange = false;  // Variable de control global
+let isUserTyping = false;  // Indica si el usuario local está escribiendo
+let typingTimeout;  // Timeout para desbloquear la escritura
+let typingStatusBarItem;  // Elemento de la barra de estado para mostrar el mensaje de escritura
+
+const outputChannel = vscode.window.createOutputChannel('Pair Programming');
+outputChannel.show();
 
 function activate(context) {
+  // Crear el StatusBarItem
+  typingStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+  typingStatusBarItem.hide();
+  context.subscriptions.push(typingStatusBarItem);
+
   const startPairProgramming = vscode.commands.registerCommand('extension.startPairProgramming', async () => {
-    const channel = supabase.channel('pair_programming', {
-      config: {
-        broadcast: {
-          ack: true
-        },
-      },
-    });
- 
-    // Suscribirse al canal
-    channel.on('broadcast', { event: 'code_change' }, (payload) => {
-      const edit = payload.payload;
-      const editor = vscode.window.activeTextEditor;
-      if (editor) {
-        isProgrammaticChange = true;  // Marcar el inicio de una edición programática
-        editor.edit(editBuilder => {
-          editBuilder.replace(new vscode.Range(
-            new vscode.Position(edit.start.line, edit.start.character),
-            new vscode.Position(edit.end.line, edit.end.character)
-          ), edit.text);
-        }).then(() => {
-          isProgrammaticChange = false;  // Marcar el final de una edición programática
-        });
-      }
+    // Mostrar opciones para crear o unirse a una sala
+    const options = ['Create a Room', 'Join a Room'];
+    const selection = await vscode.window.showQuickPick(options, {
+      placeHolder: 'Select an option'
     });
 
-    await channel.subscribe((status) => {
-      if (status === 'SUBSCRIBED') {
-        vscode.window.showInformationMessage('Connected to the pair programming channel');
-      }
-    });
+    if (selection === 'Create a Room') {
+      const roomCode = generateRoomCode();
+      vscode.window.showInformationMessage(`Room created. Share this code to join: ${roomCode}`);
+      outputChannel.appendLine(`Room code: ${roomCode}`);
 
-    const documentChangeListener = vscode.workspace.onDidChangeTextDocument(event => {
-      if (!isProgrammaticChange && channel) {
-        const edit = event.contentChanges[0];
-        channel.send({
-          type: 'broadcast',
-          event: 'code_change',
-          payload: {
-            start: {
-              line: edit.range.start.line,
-              character: edit.range.start.character
-            },
-            end: {
-              line: edit.range.end.line,
-              character: edit.range.end.character
-            },
-            text: edit.text
-          }
-        });
-      }
-    });
+      await connectToRoom(roomCode);
+    } else if (selection === 'Join a Room') {
+      const roomCode = await vscode.window.showInputBox({
+        prompt: 'Enter the room code',
+        validateInput: input => (input.length === 6 && /^\d+$/.test(input)) ? null : 'Invalid room code'
+      });
 
-    context.subscriptions.push(documentChangeListener);
+      if (roomCode) {
+        await connectToRoom(roomCode);
+      }
+    }
   });
 
   context.subscriptions.push(startPairProgramming);
+}
+
+async function connectToRoom(roomCode) {
+  const channel = supabase.channel(`pair_programming_${roomCode}`, {
+    config: {
+      broadcast: {
+        ack: true,
+      },
+    },
+  });
+
+  // Suscribirse al canal
+  channel.on('broadcast', { event: 'code_change' }, (payload) => {
+    const edit = payload.payload;
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      isProgrammaticChange = true;  // Marcar el inicio de una edición programática
+      editor.edit(editBuilder => {
+        editBuilder.replace(new vscode.Range(
+          new vscode.Position(edit.start.line, edit.start.character),
+          new vscode.Position(edit.end.line, edit.end.character)
+        ), edit.text);
+      }).then(() => {
+        isProgrammaticChange = false;  // Marcar el final de una edición programática
+      });
+    }
+  });
+
+  channel.on('broadcast', { event: 'user_typing' }, (payload) => {
+    if (payload.payload.typing && !isUserTyping) {
+      vscode.window.activeTextEditor.options.readOnly = true;
+      typingStatusBarItem.text = "A is typing...";
+      typingStatusBarItem.show();
+      outputChannel.appendLine('A is typing...');
+      clearTimeout(typingTimeout);
+    } else if (!payload.payload.typing) {
+      typingTimeout = setTimeout(() => {
+        vscode.window.activeTextEditor.options.readOnly = false;
+        typingStatusBarItem.hide();
+        outputChannel.appendLine('A stopped typing.');
+      }, 1000);
+    }
+  });
+
+  await channel.subscribe((status) => {
+    if (status === 'SUBSCRIBED') {
+      vscode.window.showInformationMessage(`Connected to the pair programming room: ${roomCode}`);
+      outputChannel.appendLine(`Connected to the pair programming room: ${roomCode}`);
+    }
+  });
+
+  const documentChangeListener = vscode.workspace.onDidChangeTextDocument(event => {
+    if (!isProgrammaticChange && channel) {
+      const edit = event.contentChanges[0];
+
+      // Enviar evento de "user_typing"
+      if (!isUserTyping) {
+        isUserTyping = true;
+        channel.send({
+          type: 'broadcast',
+          event: 'user_typing',
+          payload: {
+            typing: true
+          }
+        });
+      }
+
+      clearTimeout(typingTimeout);
+      typingTimeout = setTimeout(() => {
+        isUserTyping = false;
+        channel.send({
+          type: 'broadcast',
+          event: 'user_typing',
+          payload: {
+            typing: false
+          }
+        });
+      }, 1000);
+
+      // Enviar evento de "code_change"
+      channel.send({
+        type: 'broadcast',
+        event: 'code_change',
+        payload: {
+          start: {
+            line: edit.range.start.line,
+            character: edit.range.start.character
+          },
+          end: {
+            line: edit.range.end.line,
+            character: edit.range.end.character
+          },
+          text: edit.text
+        }
+      });
+    }
+  });
+
+  context.subscriptions.push(documentChangeListener);
+}
+
+function generateRoomCode() {
+  return Math.floor(100000 + Math.random() * 900000).toString();
 }
 
 function deactivate() {
